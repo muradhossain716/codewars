ffc1157c4338d5ba6099849ac97c8d88d66461f8
Murad <muradahamed6262@gmail.com>
2022-05-15 18:24:53 +0600

    are you  playing banjo?

---
+++ playing_banjo.js
@@ -0,0 +1,6 @@
+//are you playing banjo?
+function areYouPlayingBanjo(name) {
+    // Implement me
+    return name+(name.toLowerCase().charAt(0)==='r'? ' plays':' does not play')+' banjo';
+    
+  }
