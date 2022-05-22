5417a8c4ed5c7f39c5a61dc1cec8f0a1754ef6d7
Murad <muradahamed6262@gmail.com>
2022-05-22 16:53:07 +0600

    camel case

--- converCamelCase.js
+++ converCamelCase.js
@@ -1,8 +1,10 @@
 function toCamelCase(str){
   if(str.length>0) {
     let st=str.split('_')
+    console.log(st.length)
     
-    if(st.length<=1){st=str.split('-')}
+  if(st.length<2){st=str.split('-')
+    }
     for(i=1;i<st.length;i++){
       st[i]=st[i][0].toUpperCase()+st[i].slice(1)
       
@@ -12,5 +14,4 @@ function toCamelCase(str){
   }
   return ''
   
-}
-    console.log(toCamelCase('the_stealth_warrior'))
+}   console.log(toCamelCase('the_stealth_warrior'))
