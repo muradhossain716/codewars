973dafb8d0bd38c1d61688ff4fd7175b013727ff
Murad <muradahamed6262@gmail.com>
2022-05-22 10:15:50 +0600

    updated/refactored

--- find.js
+++ find.js
@@ -1,4 +1,10 @@
 function check(a, x) {
-    let b =a.find(n=>n==x)
-     return b==x
+    // let b =a.find(n=>n==x)
+    //  return b==x
+    return a.includes(x)
    }
+//    function check(a, x) {
+    //return a.find(n=>n==x).length>1
+//      
+//    }
+   
