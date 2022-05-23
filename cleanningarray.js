eddd0465b90643570d5eb8de14b1cbdecf864b99
Murad <muradahamed6262@gmail.com>
2022-05-23 12:50:35 +0600

    clean string

--- cleanningarray.js
+++ cleanningarray.js
@@ -2,3 +2,10 @@ function stringClean(s){
     let b=['1','2','3','4','5','6','7','8','9','0']
    return [...s].filter(x=>!b.includes(x)).join('')
   }
+
+  function isPangram(s){
+    //...
+    s=s.toLowerCase();
+    [...'abcdefghijklmnopqrstuvwxyz'].map(function(x){if(!s.includes(x))return false })
+    return true
+  }
