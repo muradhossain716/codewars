6b2078dcf136102389ec3497a0de3eadb6350baa
Murad <muradahamed6262@gmail.com>
2022-05-26 12:16:25 +0600

    another solution added of largest 5 digit in a series

--- largest5Digit.js
+++ largest5Digit.js
@@ -8,3 +8,19 @@ function solution(n){
     return max
     }
   console.log(solution('1242445'))
+
+  //another solution using str
+  function solution(digits){
+    let answer = 0;
+    
+    for (let i=0; i<digits.length; i++){
+      let number = digits.substr(i, 5);
+      console.log(number)
+       //each loop iteration pulls the next 5 digits into a substring
+      if(Number(number) > answer){       //convert to number and compare against answer
+        answer = Number(number);
+      }
+    }
+    return answer;
+}
+console.log(solution('3363078303973'))
