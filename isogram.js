84bacf1f69ffd1c4eb7cf55180d548243e1a034b
Murad <muradahamed6262@gmail.com>
2022-05-31 18:24:57 +0600

    another solution added

--- isogram.js
+++ isogram.js
@@ -1,3 +1,7 @@
 function isIsogram(str){
     return str.length===[...new Set([...(str.toLowerCase())])].length
   }
+  //another solution
+  function isIsogram(str){
+    return str.length===new Set(str.toLowerCase()).size
+  }
